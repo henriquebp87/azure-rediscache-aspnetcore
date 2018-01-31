f6194ba779f847b74f3740bb3e29f9548a1a0fbc
Henrique Pereira <heinrichpereira@hotmail.com>
2018-01-31 01:53:02 -0200

    Uploading solution files.

---
+++ AzureRedisCacheApp/AzureRedisCacheApp.WebApi/Controllers/ValuesController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+
+namespace AzureRedisCacheApp.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        private IDistributedCache _cache;
+
+        public ValuesController(IDistributedCache cache)
+        {
+            _cache = cache;
+        }
+
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            //Get or Set Cache Value
+            string value = _cache.GetString("CacheTime");
+
+            if (value == null)
+            {
+                value = DateTime.Now.ToString();
+
+                var options = new DistributedCacheEntryOptions();
+                options.SetSlidingExpiration(TimeSpan.FromMinutes(1));
+                _cache.SetString("CacheTime", value, options);
+            }
+
+            return new[] { $"Cache Time: {value}", $"Current Time: {DateTime.Now.ToString()}" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            //Get Session Value
+            return Json($"Session Time: {HttpContext.Session.GetString("SessionTime")}");
+        }
+
+        // POST api/values
+        [HttpPost]
+        public IActionResult Post([FromBody]string value)
+        {
+            //Set Session Value
+            HttpContext.Session.SetString("SessionTime", DateTime.Now.ToString());
+            return Ok(true);
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ AzureRedisCacheApp/AzureRedisCacheApp.WebApi/Startup.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.DataProtection;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace AzureRedisCacheApp.WebApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+
+            services.AddDistributedRedisCache(option =>
+            {
+                option.Configuration = Configuration.GetConnectionString("RedisConnection");
+                option.InstanceName = "master";
+            });
+
+            services.AddSession(options =>
+            {
+                //options.Cookie.HttpOnly = true;
+                options.Cookie.Name = ".ASPNetCoreSession";
+                options.IdleTimeout = TimeSpan.FromMinutes(2);
+                //options.Cookie.Path = "/";
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseSession();
+
+            app.UseMvc();
+        }
+    }
+}
